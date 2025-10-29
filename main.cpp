32775a014148c5a8851eeccd2015a72f4b0443f4
Luca Ardanaz <luca.ardanaz@gmail.com>
2025-10-29 15:34:43 -0700

    Added experimental hash function.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int main() {
-	cout << "Hello world.";
+	cout << "Hello world." << endl;
+
+	char* str = new char[20];
+
+	cout << "Enter up to 20 characters: " << endl;
+	cin.get(str, 20);
+	cin.get();
+
+	int sum = 0;
+	for (int i = 0; i < strlen(str); i++) {
+
+		sum += (int)str[i];
+
+	}
+
+	int index = sum % 100;
+
+	cout << "Index: " << index << endl;
 
 	return 0;
 }
